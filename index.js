085fd0c642eb9a1e6d9c28c9f5aedaa623e9585a
MojolaoluwaGafar <mojolaoluwa1212@gmail.com>
2025-03-07 23:19:49 +0100

    bug fix

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
@@ -22,8 +26,10 @@ app.use("/api", authRoutes);
 
 // Test route
 app.get("/", (req, res) => {
-    res.send("Backend is running!");
+  res.send("Backend is running!");
 });
 
 // Start Server
-app.listen(PORT, () => console.log(`Backend running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Backend running on http://localhost:${PORT}`)
+);
